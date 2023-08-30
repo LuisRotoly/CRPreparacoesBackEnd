0127294c13fd77e48eec3c2b07a4582705f896b0
Luis Rotoly <lu.gustavo.lima1@gmail.com>
2023-08-30 18:31:34 -0300

    Commit inicial

---
+++ src/test/java/com/crpreparacoes/crpreparacoes/CrpreparacoesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.crpreparacoes.crpreparacoes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrpreparacoesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
