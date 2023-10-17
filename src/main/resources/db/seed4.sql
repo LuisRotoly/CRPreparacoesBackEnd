7b19dd858fa1fd53a45f5c6f706a60960ec7d273
Luis Rotoly <lu.gustavo.lima1@gmail.com>
2023-10-17 13:22:14 -0300

    Alteração de tamanho de ano de moto

--- src/main/resources/db/seed4.sql
+++ src/main/resources/db/seed4.sql
@@ -1,2 +1,5 @@
 ALTER TABLE budget ADD is_removed boolean NOT NULL,
 ADD discount_percentage integer;
+
+ALTER TABLE bike MODIFY COLUMN `year` varchar(4);
+ALTER TABLE budget MODIFY COLUMN `year` varchar(4);
