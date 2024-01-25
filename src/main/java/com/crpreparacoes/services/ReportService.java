be7fdc4624f013c6abffe3ba2d719fdc9d269fa9
Luis Rotoly <lu.gustavo.lima1@gmail.com>
2024-01-25 15:44:56 -0300

    Colocando gráficos liquidez e gastos

--- src/main/java/com/crpreparacoes/services/ReportService.java
+++ src/main/java/com/crpreparacoes/services/ReportService.java
@@ -1,8 +1,11 @@
 package com.crpreparacoes.services;
 
 import com.crpreparacoes.dto.ReportDTO;
-import com.crpreparacoes.repositories.FinanceBudgetRepository;
-import com.crpreparacoes.repositories.SingleSaleFinanceRepository;
+import com.crpreparacoes.models.BikePart;
+import com.crpreparacoes.models.Budget;
+import com.crpreparacoes.models.LaborOrBikePartBudget;
+import com.crpreparacoes.models.Status;
+import com.crpreparacoes.repositories.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -20,7 +23,16 @@ public class ReportService {
     @Autowired
     private SingleSaleFinanceRepository singleSaleFinanceRepository;
 
-    public List<Double> getReportData(String year) {
+    @Autowired
+    private BudgetRepository budgetRepository;
+
+    @Autowired
+    private LaborOrBikePartBudgetRepository laborOrBikePartBudgetRepository;
+
+    @Autowired
+    private BikePartRepository bikePartRepository;
+
+    public List<Double> getGrossIncomeData(String year) {
         List<Double> receivedMoneyList = new ArrayList<>();
         List<ReportDTO> reportDTOBudgetList = financeBudgetRepository.getMonthAndValueSumFromBudget(year);
         List<ReportDTO> reportDTOSingleSaleList = singleSaleFinanceRepository.getMonthAndValueSumFromSingleSale(year);
@@ -45,5 +57,40 @@ public class ReportService {
             }
         }
         return receivedMoneyList;
+    }
+
+    public List<Double> getNetRevenueData(String year) {
+        List<Double> netRevenueList = new ArrayList<>();
+        List<Double> receivedMoneyList = getGrossIncomeData(year);
+        List<Double> spentMoneyList = getBikePartSpentData(year);
+        for (int i = 0; i < 12; i++) {
+            netRevenueList.add(receivedMoneyList.get(i)-spentMoneyList.get(i));
+        }
+        return netRevenueList;
+    }
+
+    public List<Double> getBikePartSpentData(String year) {
+        List<Double> spentMoneyList = new ArrayList<>();
+        for (int i = 0; i < 12; i++) {
+            spentMoneyList.add((double) 0);
+        }
+        List<Budget> budgetList = budgetRepository.listAllBudgetsFinishedAtYear(Status.StatusEnum.FINISHED.id, year);
+        for (Budget budget : budgetList) {
+            List<LaborOrBikePartBudget> laborOrBikePartBudgetList = laborOrBikePartBudgetRepository.findAllLaborOrBikePartBudgetById(budget.getId());
+            double bikePartSpent = 0;
+            for (LaborOrBikePartBudget laborOrBikePartBudget : laborOrBikePartBudgetList) {
+                BikePart bikePart = bikePartRepository.findByName(laborOrBikePartBudget.getName());
+                if(bikePart != null) {
+                    bikePartSpent = bikePartSpent + (bikePart.getValue() * laborOrBikePartBudget.getQuantity());
+                }
+            }
+            for (int monthCount = 0; monthCount < monthList.length; monthCount++) {
+                if((monthCount+1) == budget.getUpdatedAt().getMonth().getValue()){
+                    spentMoneyList.set(monthCount, (spentMoneyList.get(monthCount) + bikePartSpent));
+                    break;
+                }
+            }
+        }
+        return spentMoneyList;
     }
 }
